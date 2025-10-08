fa554b01aef25d04da2c3f04a38437da06abc84f
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-10-07 21:42:07 -0400

    feat: inicializando formulário de clientes

--- cliente-facil/src/app/cadastro/cadastro.component.ts
+++ cliente-facil/src/app/cadastro/cadastro.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    MatCardModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.scss'
+  styleUrl: './cadastro.component.scss',
 })
-export class CadastroComponent {
-
-}
+export class CadastroComponent {}
