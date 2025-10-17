e05eb63031d309184f32e4999cabe95d06054380
JacksonLuiz99 <jacksoncuiaba99@gmail.com>
2025-10-17 19:51:01 -0400

    feat: metodo de criar repositório

--- cliente-facil/src/app/cliente.service.ts
+++ cliente-facil/src/app/cliente.service.ts
@@ -6,10 +6,25 @@ import { Cliente } from './cadastro/cliente';
 })
 export class ClienteService {
 
+  static REPO_CLIENTES = "_CLIENTES";
+
   constructor() {
     
    }
    salvar(Cliente: Cliente) {
       console.log("Cliente salvo com sucesso:", Cliente);
+    }
+
+    obterStorage(): Cliente[] {
+      const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+      //se existir repositório
+      if(repositorioClientes) { 
+        const clientes: Cliente[] = JSON.parse(repositorioClientes);
+        return clientes;
+      }
+      //se não existir ele vai criar o repositório
+      const clientes: Cliente[] = [];
+      localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+      return clientes;
     }
 }
