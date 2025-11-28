71e60c70f3606a38f032e9ebbca646ca7dca58d2
jacksonluiz99 <jacksoncuiaba99@gmail.com>
2025-11-28 11:39:48 -0400

    Feat: Lógica apra atualizar  clientes

--- cliente-facil/src/app/cadastro/cadastro.component.ts
+++ cliente-facil/src/app/cadastro/cadastro.component.ts
@@ -8,7 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { query } from '@angular/animations';
 
 @Component({
@@ -29,7 +29,11 @@ export class CadastroComponent implements OnInit {
   cliente: Cliente = Cliente.newCliente();
   atualizando: boolean = false;
 
-  constructor(private service: ClienteService, private route: ActivatedRoute) {}
+  constructor(
+    private service: ClienteService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((query: any) => {
@@ -46,7 +50,12 @@ export class CadastroComponent implements OnInit {
   }
 
   salvar() {
-    this.service.salvar(this.cliente);
-    this.cliente = Cliente.newCliente();
+    if (!this.atualizando) {
+      this.service.salvar(this.cliente);
+      this.cliente = Cliente.newCliente();
+    } else {
+      this.service.atualizar(this.cliente);
+      this.router.navigate(['/consulta']);
+    }
   }
 }
--- cliente-facil/src/app/cliente.service.ts
+++ cliente-facil/src/app/cliente.service.ts
@@ -14,6 +14,16 @@ export class ClienteService {
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
   }
 
+  atualizar(cliente: Cliente) {
+    const storage = this.obterStorage();
+    storage.forEach((c) => {
+      if (c.id === cliente.id) {
+        Object.assign(c, cliente);
+      }
+    });
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
+  }
+
   pesquisarClientes(nomeBusca: string): Cliente[] {
     const clientes = this.obterStorage();
     if (!nomeBusca) {
@@ -32,7 +42,7 @@ export class ClienteService {
 
   buscarClientePorId(id: string): Cliente | undefined {
     const clientes = this.obterStorage();
-    return clientes.find(cliente => cliente.id === id);
+    return clientes.find((cliente) => cliente.id === id);
   }
 
   private obterStorage(): Cliente[] {
